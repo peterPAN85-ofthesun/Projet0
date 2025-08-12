c2b6c6d4284ff031e4c6b04716eb6a909826d56c
peterPAN85-ofthesun <pineaugregoire85@gmail.com>
2025-08-13 01:22:45 +0200

    startRaylib

--- Foncteur.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-using namespace std;
-
-class CompareLongueur {
-public:
-  bool operator()(const string &a, const string &b) const {
-    return a.length() < b.length();
-  }
-};
